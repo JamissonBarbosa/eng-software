af4689717043161a3611c9cf6c12aec3f92af0d4
Isaac Alfredo <isaacfreitas23@gmail.com>
2022-06-29 22:48:23 -0300

    create disciplina

--- proj-eng-software/src/application/MenuUsuarioFacade.java
+++ proj-eng-software/src/application/MenuUsuarioFacade.java
@@ -3,12 +3,29 @@ package application;
 import java.util.Scanner;
 
 public class MenuUsuarioFacade {
+	private Professor professor;
+	
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(String professorLogin, BancoDeDados bd) {
+		for (Professor professor : bd.getProfessores()) {
+			if(professor.getLogin().equals(professorLogin)) {
+				this.professor = professor;
+				break;
+			}
+		}
+	}
 
 	public MenuUsuarioFacade(){}
 	
+	
 	public void MenuProfessor(Scanner sc,BancoDeDados bd, String professorLogin) {
 		
+		this.setProfessor(professorLogin, bd);
 		boolean funcionalidadeFlag = true;
+		
 		while (funcionalidadeFlag == true) {
 			System.out.println("0- Sair\n1- Listar disciplinas\n2- Criar nova disciplina\n3- Excluir disciplina");
 			String funcionalidade = sc.nextLine();
@@ -22,13 +39,9 @@ public class MenuUsuarioFacade {
 			case "2":
 				System.out.println("Nome da disciplina: ");
 				String nomeDaDisciplina = sc.nextLine();
-				
-				for (Professor professor : bd.getProfessores()) {
-					if(professor.getLogin().equals(professorLogin)) {
-						Disciplina disciplina = new Disciplina(nomeDaDisciplina,professor.getNome());
-						professor.getDisciplinas().add(disciplina);
-					}
-				}
+				Disciplina disciplina = new Disciplina(nomeDaDisciplina,this.professor.getNome());
+				this.professor.getDisciplinas().add(disciplina);
+				System.out.println("Disciplina criada com sucesso!");
 				break;
 			case "3":
 				break;
